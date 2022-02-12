f1161319ab9accc7c0da2290a303917fa22e83c4
Conor Murphy <cmurphy580@gmail.com>
2022-02-12 09:47:53 -0800

    stack with news api working

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/js', express.static(__dirname + 'public/js'));
+
+// Templating engine
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// Routes
+const newsRouter = require('./src/routes/news');
+app.use('/', newsRouter);
+
+
+// Port 3000
+app.listen(port, () => console.log(`Port: ${port}`));
---
+++ src/routes/news.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const newsRouter = express.Router();
+const axios = require('axios');
+
+newsRouter.get('', async (req, res) => {
+  try {
+    const API_KEY = '0c224bd6f6fc43fb9e28d804bde31f57';
+    const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    // console.log(news.data.articles);
+    res.render('news', { articles: news.data.articles });
+  }
+  catch(error) {
+    console.log("Error: ", error.message);
+  }
+});
+
+module.exports = newsRouter;
