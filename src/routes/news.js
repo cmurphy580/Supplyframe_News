ac470729ac9366279cfd432a8eebb7ca4e36b6d7
Conor Murphy <cmurphy580@gmail.com>
2022-02-13 10:03:55 -0800

    home page css

--- src/routes/news.js
+++ src/routes/news.js
@@ -3,15 +3,28 @@ const newsRouter = express.Router();
 const axios = require('axios');
 
 newsRouter.get('', async (req, res) => {
-  try {
+  /*
+  window.addEventListener('scroll', (event) => {
+    if (window.scrollTo() > 60) 
+    {
+      document.body.header.style.backgroundColor = "#1E1E1E";
+    }
+    else
+    {
+      document.body.header.style.backgroundColor = "rgba(255, 255, 255, 0.0)";
+    }
+  }); 
+  */
+  try 
+  {
     const API_KEY = '0c224bd6f6fc43fb9e28d804bde31f57';
     const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    // console.log(news.data.articles);
+    console.log(news.data.articles);
     res.render('news', { articles: news.data.articles });
   }
-  catch(error) {
+  catch(error) 
+  {
     console.log("Error: ", error.message);
   }
 });
-
 module.exports = newsRouter;
