46a56ef2847e7191b65de324a0ba652d21ea74cb
Conor Murphy <cmurphy580@gmail.com>
2022-02-13 10:54:11 -0800

    home page pagination

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,8 @@ app.use('/js', express.static(__dirname + 'public/js'));
 // Templating engine
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended : true }));
 
 // Routes
 const newsRouter = require('./src/routes/news');
--- src/routes/news.js
+++ src/routes/news.js
@@ -19,8 +19,34 @@ newsRouter.get('', async (req, res) => {
   {
     const API_KEY = '0c224bd6f6fc43fb9e28d804bde31f57';
     const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    console.log(news.data.articles);
-    res.render('news', { articles: news.data.articles });
+    // console.log(news.data.articles.length);
+    res.render('news', { articles: news.data.articles, url: false });
+  }
+  catch(error) 
+  {
+    console.log("Error: ", error.message);
+  }
+});
+
+newsRouter.get('/page_two', async (req, res) => {
+  /*
+  window.addEventListener('scroll', (event) => {
+    if (window.scrollTo() > 60) 
+    {
+      document.body.header.style.backgroundColor = "#1E1E1E";
+    }
+    else
+    {
+      document.body.header.style.backgroundColor = "rgba(255, 255, 255, 0.0)";
+    }
+  }); 
+  */
+  try 
+  {
+    const API_KEY = '0c224bd6f6fc43fb9e28d804bde31f57';
+    const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    console.log(req.url);
+    res.render('news', { articles: news.data.articles, url: req.url });
   }
   catch(error) 
   {
